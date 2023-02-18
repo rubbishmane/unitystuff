d05ff507834545ceea045871952c6eee4995b666
rubbishmane <107376193+rubbishmane@users.noreply.github.com>
2023-02-18 23:34:06 +1300

    First Attempt at player movement

--- testing_facility/Assets/playerMovement.cs
+++ testing_facility/Assets/playerMovement.cs
@@ -5,18 +5,58 @@ using UnityEngine;
 public class playerMovement : MonoBehaviour
 {
     Rigidbody rb;
+    public float speed;
+    //public float maxMoveSpeed;
+    public float moveX, moveY;
+    
     void Start()
     {
         rb = gameObject.GetComponent<Rigidbody>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.H))
-        {
-            rb.AddForce(Vector3.forward, ForceMode.Impulse);
-        }
+        moveX = Input.GetAxis("Horizontal");
+        moveY = Input.GetAxis("Vertical");
+        
+
+        
         
     }
+
+    void FixedUpdate()
+    {
+        PlayerMove();
+        
+
+        //Clamp Player Speed
+        // if(rb.velocity.magnitude >= maxMoveSpeed)
+        // {
+        //     rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxMoveSpeed);
+        // }
+
+    }
+
+    void PlayerMove()
+    {
+        
+        
+
+        rb.AddForce(new Vector3(moveX, 0, moveY), ForceMode.Impulse);
+        if(moveX == 0)
+        {
+            rb.velocity = new Vector3(0,0,moveY);
+        }
+        if(moveY == 0)
+        {
+            rb.velocity = new Vector3(moveX,0,0);
+        }
+
+        
+
+    }
 }
+
+
