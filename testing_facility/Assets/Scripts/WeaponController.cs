f04253a2ca52321f50c66c6c3b8a337ff9cf2a19
rubbishmane <107376193+rubbishmane@users.noreply.github.com>
2023-02-20 11:02:40 +1300

    more stuff

---
+++ testing_facility/Assets/Scripts/WeaponController.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+    public GameObject stick, fish, hand, glock, sniper;
+    [SerializeField] GameObject player;
+}
